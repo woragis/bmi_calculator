58508905f49813efeaf6d78b00ba47d2e38a805a
woragis <masteringthecode.woragis+github@gmail.com>
2024-12-20 22:20:20 -0300

    Make working bmi calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface UserData {
+    height: number;
+    weight: number;
+  }
+  const [userData, setUserData] = useState<UserData>({} as UserData);
+  const [bmi, setBmi] = useState<number>(0);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserData(
+      (prevState) =>
+        (prevState = { ...prevState, [event.target.name]: event.target.value })
+    );
+    setBmi((userData.weight * 100) / userData.height ** 2);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <label htmlFor="height">
+        Height (in centimeters):
+        <input
+          type="number"
+          name="height"
+          id="height"
+          value={userData.height}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label htmlFor="weight">
+        Weight (in kg):
+        <input
+          type="number"
+          name="weight"
+          id="weight"
+          value={userData.weight}
+          onChange={handleChange}
+        />
+      </label>
+      <div className="result">
+        <p>Your BMI: {bmi && bmi}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
